c6eedc4e1417cb052c902acee8b167f6cc2c95b2
Joshua van Amerom <joshua.vanamerom@kcl.ac.uk>
2015-08-21 14:48:07 +0100

    isolate changes to warning status to function

--- Displaying Results/Metric_Landscape.m
+++ Displaying Results/Metric_Landscape.m
@@ -3,4 +3,5 @@ warning off; %#ok<*WNOFF>
 Interpolated_Landscape = TriScatteredInterp(Optimization.hr1s',Optimization.hr2s',double(Optimization.metrix'));
 [XI,YI] = meshgrid(1:71,1:71);
 Landscape=Interpolated_Landscape(XI,YI);
+warning on; 
 end
